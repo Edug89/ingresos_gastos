0ab78ffa1072f0c5b3b5fbe742f5ac7c25dceea0
Your Name <you@example.com>
2022-09-19 10:58:14 +0200

    Nueva versión, añadimos fichas sin terminar de modifiacion y baja

--- registro_ig/routes.py
+++ registro_ig/routes.py
@@ -6,6 +6,14 @@ from flask import render_template
 def index():
     return render_template("index.html", pageTitle="Lista", movimientos=[])
 
-@app.route("/nuevo")
+@app.route("/alta")
 def alta():
     return render_template("new.html", pageTitle="Alta")
+
+@app.route("/baja")
+def baja():
+    return render_template("baja.html", pageTitle="Baja")
+
+@app.route("/modif")
+def modificacion():
+    return render_template("modif.html", pageTitle="Modificacion")
